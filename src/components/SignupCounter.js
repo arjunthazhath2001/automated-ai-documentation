31fdc1889a40dee05d0876b9a9d6918eb39f9480
arjunthazhath2001 <arjunta32@gmail.com>
2025-08-23 10:33:22 +0530

    ok

--- src/components/SignupCounter.js
+++ src/components/SignupCounter.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 
 const SignupCounter = ({ className = '' }) => {
-  const [count, setCount] = useState(92)
+  const [count, setCount] = useState(1092)
   const [isVisible, setIsVisible] = useState(false)
 
   useEffect(() => {
